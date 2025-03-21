dc107145c805ee06b1d1cba25f3a62b25d710451
VEMA <144062503+vemadevelopers@users.noreply.github.com>
2025-03-21 19:43:22 +0000

    nn

---
+++ components/Loader.tsx
@@ -0,0 +1,12 @@
+// components/Loader.js
+import styles from '../styles/Loader.module.css';
+
+const Loader = () => {
+  return (
+    <div className={styles.loaderContainer}>
+      <div className={styles.loader}></div>
+    </div>
+  );
+};
+
+export default Loader;
